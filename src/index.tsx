c92b993658cf1ae44015bfcbe4d5142daae51d67
Khushal7773 <khushalsinghpanwar07@gmail.com>
2023-08-29 12:44:59 +0530

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,3 @@
-// import { createRoot } from "react-dom/client";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-// import { BrowserRouter } from "react-router-dom";
-// import {
-//   CssBaseline,
-//   ThemeProvider,
-//   createTheme,
-//   StyledEngineProvider,
-// } from "@mui/material";
-
-// import "./StyleSheets/global.css";
-
-// const muiTheme = createTheme();
-
-// const container = document.getElementById("root");
-// const root = createRoot(container!);
-
-// root.render(
-//   <BrowserRouter>
-//     <StyledEngineProvider injectFirst>
-//       <ThemeProvider theme={muiTheme}>
-//         <CssBaseline />
-//         <App />
-//       </ThemeProvider>
-//     </StyledEngineProvider>
-//   </BrowserRouter>
-// );
-
-// reportWebVitals();
-
-
-
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
